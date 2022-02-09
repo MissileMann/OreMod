f9e19e0a1fa81d3e4bd1d215ab916b7bfe6f81ea
MissileMann <misael.escoto043@gmail.com>
2022-02-08 23:54:18 -0600

    New textures, loot tables, recipe and new block
    New textures for all blocks and items. Added Ebonite Block. Added Loot
    Table to Ebonite Ore. Recipe to turn Ebonite Gems into Blocks and back.

--- src/main/java/com/missile/oremod/core/init/BlockInit.java
+++ src/main/java/com/missile/oremod/core/init/BlockInit.java
@@ -16,11 +16,19 @@ public class BlockInit {
 
 	public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, OreMod.MOD_ID);
 	
-	public static final RegistryObject<Block> ORE_BLOCK = BLOCKS
-			.register("ore_block", 
+	public static final RegistryObject<Block> EBONITE_ORE = BLOCKS
+			.register("ebonite_ore", 
 			() -> new Block(AbstractBlock.Properties.of(Material.METAL, MaterialColor.COLOR_PURPLE)
 					.strength(15f, 30f)
 					.harvestLevel(3)
 					.harvestTool(ToolType.PICKAXE)
 					.sound(SoundType.ANCIENT_DEBRIS)));
+	
+	public static final RegistryObject<Block> EBONITE_BLOCK = BLOCKS
+			.register("ebonite_block", 
+					() -> new Block(AbstractBlock.Properties.of(Material.METAL, MaterialColor.COLOR_PURPLE)
+							.strength(15f, 30f)
+							.harvestLevel(2)
+							.harvestTool(ToolType.PICKAXE)
+							.sound(SoundType.METAL)));
 }
